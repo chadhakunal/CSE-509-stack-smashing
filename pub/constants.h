1fa61ed17e6983084e29ec3358773fcf4843c505
chadhakunal <chadhakunal1224@gmail.com>
2024-09-23 06:36:02 -0400

    fixes

--- pub/constants.h
+++ pub/constants.h
@@ -3,14 +3,14 @@
 
 const char *nargv[3] = { "vuln", STRINGIFY(GRP), NULL };
 
-const uint64_t auth_bp          =   0x7ffca4e76e10; // rbp value in auth
-const uint64_t auth_cred_loc    =   0x7ffca4e76e00; // loc of cred 
-const uint64_t auth_db_loc      =   0x7ffca4e76df8; // loc of db (local var of auth)
-const uint64_t g_bp             =   0x7ffca4e76e60; // rbp value in g
-const uint64_t auth_cred        =   0x7ffca4e76d10; // value of cred (after alloca)
-const uint64_t main_bp          =   0x7ffca4e77550; // saved rbp value in mainloop
-uint64_t main_loop_return_addr  =   0x5613287a3ea9; // return address on the stack inside main loop
-uint64_t private_helper_addr    =   0x5613287a3f3a;
+const uint64_t auth_bp                  =   0x7ffca4e76e10; // rbp value in auth
+const uint64_t auth_cred_loc            =   0x7ffca4e76e00; // loc of cred 
+const uint64_t auth_db_loc              =   0x7ffca4e76df8; // loc of db (local var of auth)
+const uint64_t g_bp                     =   0x7ffca4e76e60; // rbp value in g
+const uint64_t auth_cred                =   0x7ffca4e76d10; // value of cred (after alloca)
+const uint64_t main_bp                  =   0x7ffca4e77550; // saved rbp value in mainloop
+const uint64_t main_loop_return_addr    =   0x5613287a3ea9; // return address on the stack inside main loop
+const uint64_t private_helper_addr      =   0x5613287a3f3a;
 
 const uint64_t auth_bp_cred_loc_dist    = auth_cred_loc - auth_bp;
 const uint64_t auth_db_cred_dist        = auth_db_loc - auth_cred;
